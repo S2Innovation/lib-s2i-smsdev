eb5c9a8a77f7b8256e0c9619a791fea1bd1f3218
Lukasz Zytniak <lzytniak@gmail.com>
2018-06-12 09:30:25 -0400

    test update

--- script/test.py
+++ script/test.py
@@ -6,14 +6,9 @@
 # Imports
 import sys, os
 sys.path.insert(0, os.getcwd())
-from smsmodem import SmsSender, SmsError
+from smsmodem import SmsSender
 
 
 # Script
 sender = SmsSender("130.235.94.141")
-mid, load = sender.send_sms(["+33781242662"], u"Café à la crème!")
-status = sender.get_status(mid)
-print mid, load, status
-try: print sender.get_status(0)
-except SmsError as e:
-    print repr(e), str(e)
+sender.send_sms("+33781242662", u"Café à la crème!")
