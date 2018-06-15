08fc0249ccff26b9a0ecf10ab100707943b89af4
Piotr Goryl <piotr.goryl@s2innovation.com>
2018-06-15 16:05:27 +0200

    addjustin version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "panic-smslib",
-      version = "0.1",
+      version = "0.9",
       description = "Provide an interface for SMSGateway device server",
       packages = ['smslib'],
      )
